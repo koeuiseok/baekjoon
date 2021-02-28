d339cf02551c673faa814dfafaa216a316e5da4e
euiseok Ko <y1201k@kookmin.ac.kr>
2021-03-01 05:51:41 +0900

    21-03-01
    21-03-01

---
+++ step 1/2675.c
@@ -0,0 +1,44 @@
+
+/**************************************************************
+문제
+문자열 S를 입력받은 후에, 각 문자를 R번 반복해 새 문자열 P를 만든 후 출력하는 프로그램을 작성하시오. 즉, 첫 번째 문자를 R번 반복하고, 두 번째 문자를 R번 반복하는 식으로 P를 만들면 된다. S에는 QR Code "alphanumeric" 문자만 들어있다.
+
+QR Code "alphanumeric" 문자는 0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ\$%*+-./: 이다.
+
+입력
+첫째 줄에 테스트 케이스의 개수 T(1 ≤ T ≤ 1,000)가 주어진다. 각 테스트 케이스는 반복 횟수 R(1 ≤ R ≤ 8), 문자열 S가 공백으로 구분되어 주어진다. S의 길이는 적어도 1이며, 20글자를 넘지 않는다.
+
+출력
+각 테스트 케이스에 대해 P를 출력한다.
+**************************************************************/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+	int ret,i, temp;
+	long inlen;
+	int num = 0;
+	char input[21];
+	
+	ret = scanf("%d", &num);
+	for (i = 0; i < num; i++) {
+		ret = scanf("%d", &temp);
+		ret = scanf("%s", input);
+
+		for (int cnt_i = 0; cnt_i < strlen(input); cnt_i++) {
+			for (int j = 0; j < temp; j++) {
+				printf("%c", input[cnt_i]);
+			}
+		}
+		printf("\n");
+		
+	}
+	return 0;
+}
+
+
