7a8362908370f79062b425f39fd2eca44792e5a1
euiseok Ko <y1201k@kookmin.ac.kr>
2021-04-01 09:39:04 +0900

    Update 1002.cpp

--- cpp/step 3/1002.cpp
+++ cpp/step 3/1002.cpp
@@ -17,6 +17,17 @@ int main()
 		cin >> x1 >> y1 >> r1 >> x2 >> y2 >> r2;
 		d = sqrt(pow(x1-x2,2)+pow(y1-y2,2));
 		r = (r1 > r2) ? r2 : r1;
+		
+		if(r1==0|| r2==0){
+			if (x1 == x2&&y1==y2) {
+				cout << "-1" << endl;
+			}
+			else {
+				cout << "0" << endl;
+			}
+			continue;
+		}
+		
 		if (x1 == x2 && y1 == y2) {
 			if (r1 == r2) {
 				cout << "-1" << endl;
